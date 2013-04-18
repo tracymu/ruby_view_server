2d28944e19d0e9098e2afbee9f6f1561d469c89e
TracyMu <tracy.musung@moomumedia.com>
2013-04-18 19:44:45 +1000

    homework section 1 I added some erb things

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,16 @@
 require 'erb'
 
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+template = ERB.new "The value of x is: <%= x %> but I don't really care but x is <%= x %>"
 puts template.result(binding)
+
+tracy_hash = {:name => "Tracy", :age => 32}
+
+about_tracy = ERB.new "<%=tracy_hash[:name]%> is <%= tracy_hash[:age]%> years old"
+puts about_tracy.result(binding)
+
+aidan_string = "This is the Aidan string"
+
+about_aidan = ERB.new "What is this? <%=aidan_string%>"
+
+puts about_aidan.result(binding)
